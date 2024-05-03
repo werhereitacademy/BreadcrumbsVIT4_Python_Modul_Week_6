06e1fdf3d172977da61e3849897ef72871ca8b97
ksmkudret <ksmkudret@gmail.com>
2024-05-03 22:31:53 +0200

    Q.3

--- Hackerrank_solutions.py
+++ Hackerrank_solutions.py
@@ -28,7 +28,19 @@ if __name__ == '__main__':
 
 
 
-#Question 3:
+#Question 3: (Mutations)
+
+def mutate_string(string, position, character):
+    Mutated = string[:position] + character + string[position+1:]
+    return Mutated
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
 
 
 
