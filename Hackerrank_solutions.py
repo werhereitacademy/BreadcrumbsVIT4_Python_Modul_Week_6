5f66736bf9fd8cf5fe9552b7bed5770cf532393d
ERKHAN KOZHA <erkhankozha@gmail.com>
2024-05-03 11:49:15 +0200

    String Split & Join is done

--- Hackerrank_solutions.py
+++ Hackerrank_solutions.py
@@ -15,6 +15,15 @@ if __name__ == '__main__':
 
 
 #Question 2:
+def split_and_join(line):
+    # write your code here
+    return '-'.join(line.split())
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
 
 
 
